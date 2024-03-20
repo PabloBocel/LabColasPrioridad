02d7e71857f31899b57b2039d01c4b421590cbe6
Christopher Yuman <chrisyumanv@gmail.com>
2024-03-20 08:22:25 -0600

    Diseño y Muestra List llegada

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,6 +42,8 @@
             this.label8 = new System.Windows.Forms.Label();
             this.btnOrdenOriginal = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.LBListaPacientes = new System.Windows.Forms.ListBox();
             this.CBEnfermedades = new System.Windows.Forms.ComboBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
@@ -52,9 +54,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(346, 21);
+            this.label1.Location = new System.Drawing.Point(461, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(194, 24);
+            this.label1.Size = new System.Drawing.Size(248, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Informacion pacientes";
             // 
@@ -62,43 +65,48 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(34, 45);
+            this.label2.Location = new System.Drawing.Point(7, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(156, 24);
+            this.label2.Size = new System.Drawing.Size(199, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre paciente";
             // 
             // TBnombre
             // 
-            this.TBnombre.Location = new System.Drawing.Point(38, 82);
+            this.TBnombre.Location = new System.Drawing.Point(13, 101);
+            this.TBnombre.Margin = new System.Windows.Forms.Padding(4);
             this.TBnombre.Name = "TBnombre";
-            this.TBnombre.Size = new System.Drawing.Size(100, 20);
+            this.TBnombre.Size = new System.Drawing.Size(132, 22);
             this.TBnombre.TabIndex = 2;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(313, 45);
+            this.label4.Location = new System.Drawing.Point(226, 55);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(218, 24);
+            this.label4.Size = new System.Drawing.Size(277, 29);
             this.label4.TabIndex = 5;
             this.label4.Text = "Enfermedad de paciente";
             // 
             // TBSangre
             // 
-            this.TBSangre.Location = new System.Drawing.Point(38, 185);
+            this.TBSangre.Location = new System.Drawing.Point(13, 228);
+            this.TBSangre.Margin = new System.Windows.Forms.Padding(4);
             this.TBSangre.Name = "TBSangre";
-            this.TBSangre.Size = new System.Drawing.Size(100, 20);
+            this.TBSangre.Size = new System.Drawing.Size(132, 22);
             this.TBSangre.TabIndex = 8;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(34, 148);
+            this.label5.Location = new System.Drawing.Point(7, 182);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(138, 24);
+            this.label5.Size = new System.Drawing.Size(177, 29);
             this.label5.TabIndex = 7;
             this.label5.Text = "Tipo de sangre";
             // 
@@ -106,9 +114,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(77, 424);
+            this.label6.Location = new System.Drawing.Point(103, 522);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(166, 24);
+            this.label6.Size = new System.Drawing.Size(213, 29);
             this.label6.TabIndex = 9;
             this.label6.Text = "Siguiente paciente";
             // 
@@ -116,28 +125,31 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(288, 424);
+            this.label7.Location = new System.Drawing.Point(384, 522);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(274, 24);
+            this.label7.Size = new System.Drawing.Size(351, 29);
             this.label7.TabIndex = 11;
             this.label7.Text = "Orden de pacientes priorizados";
             this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // BTsiguiente
             // 
-            this.BTsiguiente.Location = new System.Drawing.Point(121, 459);
+            this.BTsiguiente.Location = new System.Drawing.Point(161, 565);
+            this.BTsiguiente.Margin = new System.Windows.Forms.Padding(4);
             this.BTsiguiente.Name = "BTsiguiente";
-            this.BTsiguiente.Size = new System.Drawing.Size(75, 23);
+            this.BTsiguiente.Size = new System.Drawing.Size(100, 28);
             this.BTsiguiente.TabIndex = 13;
-            this.BTsiguiente.Text = "Siguiente";
+            this.BTsiguiente.Text = "Mostrar";
             this.BTsiguiente.UseVisualStyleBackColor = true;
             this.BTsiguiente.Click += new System.EventHandler(this.BTsiguiente_Click);
             // 
             // BTorden
             // 
-            this.BTorden.Location = new System.Drawing.Point(382, 459);
+            this.BTorden.Location = new System.Drawing.Point(509, 565);
+            this.BTorden.Margin = new System.Windows.Forms.Padding(4);
             this.BTorden.Name = "BTorden";
-            this.BTorden.Size = new System.Drawing.Size(75, 23);
+            this.BTorden.Size = new System.Drawing.Size(100, 28);
             this.BTorden.TabIndex = 14;
             this.BTorden.Text = "Mostrar";
             this.BTorden.UseVisualStyleBackColor = true;
@@ -147,48 +159,69 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(626, 424);
+            this.label8.Location = new System.Drawing.Point(835, 522);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(157, 24);
+            this.label8.Size = new System.Drawing.Size(201, 29);
             this.label8.TabIndex = 15;
             this.label8.Text = "Orden de llegada";
             this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // btnOrdenOriginal
             // 
-            this.btnOrdenOriginal.Location = new System.Drawing.Point(676, 462);
-            this.btnOrdenOriginal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOrdenOriginal.Location = new System.Drawing.Point(901, 569);
+            this.btnOrdenOriginal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnOrdenOriginal.Name = "btnOrdenOriginal";
-            this.btnOrdenOriginal.Size = new System.Drawing.Size(56, 19);
+            this.btnOrdenOriginal.Size = new System.Drawing.Size(75, 23);
             this.btnOrdenOriginal.TabIndex = 16;
             this.btnOrdenOriginal.Text = "Mostrar";
             this.btnOrdenOriginal.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnRegistrar);
+            this.groupBox1.Controls.Add(this.LBListaPacientes);
             this.groupBox1.Controls.Add(this.CBEnfermedades);
-            this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.TBnombre);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.TBSangre);
-            this.groupBox1.Location = new System.Drawing.Point(23, 94);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Location = new System.Drawing.Point(31, 116);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(843, 266);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(1256, 327);
             this.groupBox1.TabIndex = 17;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Formulario";
             // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(287, 199);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 12;
+            this.btnRegistrar.Text = "Guardar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // LBListaPacientes
+            // 
+            this.LBListaPacientes.FormattingEnabled = true;
+            this.LBListaPacientes.ItemHeight = 16;
+            this.LBListaPacientes.Location = new System.Drawing.Point(523, 36);
+            this.LBListaPacientes.Name = "LBListaPacientes";
+            this.LBListaPacientes.Size = new System.Drawing.Size(712, 244);
+            this.LBListaPacientes.TabIndex = 11;
+            // 
             // CBEnfermedades
             // 
             this.CBEnfermedades.FormattingEnabled = true;
-            this.CBEnfermedades.Location = new System.Drawing.Point(316, 82);
-            this.CBEnfermedades.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CBEnfermedades.Location = new System.Drawing.Point(230, 101);
+            this.CBEnfermedades.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.CBEnfermedades.Name = "CBEnfermedades";
-            this.CBEnfermedades.Size = new System.Drawing.Size(100, 21);
+            this.CBEnfermedades.Size = new System.Drawing.Size(218, 24);
             this.CBEnfermedades.TabIndex = 10;
             // 
             // pictureBox1
@@ -196,19 +229,20 @@
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.pictureBox1.Location = new System.Drawing.Point(607, 59);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(1313, 171);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(158, 183);
+            this.pictureBox1.Size = new System.Drawing.Size(211, 225);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 531);
+            this.ClientSize = new System.Drawing.Size(1536, 654);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnOrdenOriginal);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.BTorden);
@@ -217,6 +251,7 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Centro medico";
             this.groupBox1.ResumeLayout(false);
@@ -244,6 +279,8 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ComboBox CBEnfermedades;
+        private System.Windows.Forms.ListBox LBListaPacientes;
+        private System.Windows.Forms.Button btnRegistrar;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,16 @@ namespace LabColasPrioridad
 {
     public partial class Form1 : Form
     {
+        private List<Paciente> pacientes = new List<Paciente>();
+
         public Form1()
         {
             InitializeComponent();
+            CBEnfermedades.Items.Add("Fiebre alta");
+            CBEnfermedades.Items.Add("Fractura expuesta");
+            CBEnfermedades.Items.Add("Intoxicación");
+            CBEnfermedades.Items.Add("Dolor de oído");
+            CBEnfermedades.Items.Add("Problema cardiaco");
         }
 
         private void BTsiguiente_Click(object sender, EventArgs e)
@@ -36,5 +43,53 @@ namespace LabColasPrioridad
         {
 
         }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            // Captura el nombre, enfermedad y tipo de sangre del formulario
+            string nombre = TBnombre.Text;
+            string enfermedad = CBEnfermedades.SelectedItem.ToString();
+            string tipoSangre = TBSangre.Text;
+
+            // Crea un nuevo paciente y agrega la hora actual como tiempo de registro
+            Paciente nuevoPaciente = new Paciente(nombre, enfermedad, tipoSangre, DateTime.Now);
+            pacientes.Add(nuevoPaciente);
+
+            // Ordena la lista de pacientes por tiempo de registro
+            pacientes = pacientes.OrderBy(p => p.TiempoRegistro).ToList();
+
+            // Actualiza el ListBox con la lista de pacientes
+            ActualizarListBoxPacientes();
+        }
+        private void ActualizarListBoxPacientes()
+        {
+            // Limpia el ListBox
+            LBListaPacientes.Items.Clear();
+
+            // Agrega cada paciente al ListBox
+            foreach (var paciente in pacientes)
+            {
+                LBListaPacientes.Items.Add($"Nombre: {paciente.Nombre}, Enfermedad: {paciente.Enfermedad}, Tipo de Sangre: {paciente.TipoSangre}, Tiempo de Registro: {paciente.TiempoRegistro}");
+            }
+        }
+    }
+
+    public class Paciente
+    {
+        public string Nombre { get; }
+        public string Enfermedad { get; }
+        public string TipoSangre { get; }
+        public DateTime TiempoRegistro { get; }
+
+        public Paciente(string nombre, string enfermedad, string tipoSangre, DateTime tiempoRegistro)
+        {
+            
+            Nombre = nombre;
+            Enfermedad = enfermedad;
+            TipoSangre = tipoSangre;
+            TiempoRegistro = tiempoRegistro;
+        }
     }
 }
+    
+
